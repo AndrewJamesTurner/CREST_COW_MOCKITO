8aea78470b233ff16bf6c70ebbf434363e1f63ab
hdg7 <menendez@viru1.(none)>
2016-04-14 11:51:07 +0100

    Last version of the GA

--- gen.r
+++ gen.r
@@ -15,11 +15,19 @@ maxv = rep(1,dimen)
 
 fitness <- function(population)
 {
-	arguments = paste(as.character(files[which(as.logical(population), arr.ind = TRUE, useNames = TRUE),1]),collapse=' ')
-	print(arguments)
-	system(paste("sh fitnessFunctio.sh",arguments,sep=" "))
-	val <- read.table("sol.txt",sep=",",head=FALSE)
-	val[1,1]
+	if(sum(population)>900)
+	{
+		0
+	}
+	else
+	{
+		arguments = paste(as.character(files[which(as.logical(population), arr.ind = TRUE, useNames = TRUE),1]),collapse=' ')
+		print(arguments)
+		time <- system.time(system(paste("sh fitnessFunctio.sh",arguments,sep=" ")))
+		val <- read.table("sol.txt",sep=",",head=FALSE)
+		print(paste("Solution = ",val[1,1],sum(population),time[[1]],sep=" "))
+		1.5*val[1,1]-0.5*sum(population)/length(files[,1])-time[[1]]/40
+	}
 }
-GA <- ga(type="binary",fitness=fitness,nBits=dimen,popSize=2)
+GA <- ga(type="binary",fitness=fitness,nBits=dimen,popSize=5)
 print(GA)
